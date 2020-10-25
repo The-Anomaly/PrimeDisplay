900ac06e935e89c5fadd342f46ab1878f377778a
AlexanderOba <alexzy4ak@gmail.com>
2020-10-25 09:12:52 -0700

    completed payment page

--- src/Components/Home/Privacy_policy_page/privacy_policy.tsx
+++ src/Components/Home/Privacy_policy_page/privacy_policy.tsx
@@ -23,7 +23,7 @@ const Privacy=()=>{
                              Yudimy (“we,” “our,” or “us”) is committed to protecting your privacy.  This Privacy Policy explains how your personal information is collected, used, and disclosed by Yudimy.
 This Privacy Policy applies to our website, and its associated subdomains (collectively, our “Service”) alongside our application, CLARITY. By accessing or using our Service, you signify that you have read, understood, and agree to our collection, storage, use, and disclosure of your personal information as described in this Privacy Policy and our Terms of Service.
  <br></br><br></br>
-Meanings
+<strong>Meanings</strong>
 <br></br><br></br>  
 
 <strong>For this Privacy Policy:</strong>
--- src/Components/Home/Redesigned_Payment_Page/payment_page.tsx
+++ src/Components/Home/Redesigned_Payment_Page/payment_page.tsx
@@ -1,37 +1,90 @@
 import React from 'react'
 import { Container, Row, Col, Card} from 'react-bootstrap';
-import { Link } from 'react-router-dom'
-import  mark  from '../../../assets/mark-icn.png'
+import { Link } from 'react-router-dom';
+import  mark  from '../../../assets/mark-icn.png';
+import mark_blue from  '../../../assets/blue-mark.png';
+import mark_green from '../../../assets/green-mark.png';
+import './payment.css';
 
 const Paymentpage=()=>{
     return(
         <div>
-            <div className="section-one">
+            <div className="payment-section">
                 <Container>
-                    <Row>
-                        <Col md={4}>
-                            <Card className="h-300 shadow-sm bg-white rounded">
-                               <Card.Header>
-                                   <strong>N0.00</strong> <span>/one-off</span>
+                    <div className="payment-decription"><h3>We have curated payments plans with you in mind</h3></div>
+                    <Row className="centered_payment">
+                        <Col md={3} className="margined_col">
+                            <Card className="h-300 shadow-sm bg-white payment-card">
+                               <Card.Header className="payment-header">
+                                   <h4>N0.00  <span>/one-off</span></h4> 
                                </Card.Header>
                                <Card.Body>
-                                   <div>
+                                   <div className="card-div">
                                      <h6>Free</h6>
                                      <p>pilnar pivinar tempus o tempor impermjidiet mattis acumsan nulia vel</p>
                                    </div>
-                                   <div>
-                                       <div className="flexed-div"><img src={mark} /><p>Career fitness score</p></div>
-                                       <div className="flexed-div"><img src={mark}/><p>Career personality type</p></div>
-                                       <div className="flexed-div"><img src={mark}/><p>Cv builder</p></div>
-                                   </div>
+                                   <ul className="card-list">
+                                       <li><img src={mark} className="card-image" />Career fitness score</li>
+                                       <li><img src={mark} className="card-image"/>Career personality type</li>
+                                       <li><img src={mark} className="card-image"/>Cv builder</li>
+                                   </ul>
+                                   <span className="card_btn btn-yellow">Subscribe</span>
                                </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={4}>
-
+                        <Col md={3} sm={9} className="margined_col">
+                         <Card className=" shadow-sm bg-white payment-card">
+                              <Card.Header className="payment-header">
+                                   <h4>N3500  <span>/one-off</span></h4> 
+                               </Card.Header>
+                               <Card.Body>
+                                   <div className="card-div ">
+                                     <h6 className="blue">Insight Plan</h6>
+                                     <p>Quis cras adipiscing vestibulum ut praesent lorem aliquam dictum habitant.</p>
+                                   </div>
+                                   <ul className="card-list">
+                                       <li><img src={mark_blue} className="card-image" />Everything under <span>FREE</span></li>
+                                       <li><img src={mark_blue} className="card-image"/>Strenght & Weaknes Report</li>
+                                       <li><img src={mark_blue} className="card-image"/>Competence analysis Report </li>
+                                       <li><img src={mark_blue} className="card-image"/> Most suitable career business expression </li>
+                                       <li><img src={mark_blue} className="card-image"/>Report on best roles to apply </li>
+                                       <li><img src={mark_blue} className="card-image"/>Career drivers and how to leverage them</li>
+                                       <li><img src={mark_blue} className="card-image"/>Highlights on areas for career optimization.</li>
+                                   </ul>
+                                   <span className="card_btn btn-blue">Upgrade to Insight</span>
+                               </Card.Body>
+                            </Card>
                         </Col>
-                        <Col md={4}>
-
+                        <Col md={3} sm={9} className="margined_col">
+                        <Card className="h-300 shadow-sm bg-white payment-card">
+                               <Card.Header className="payment-header">
+                                   <h4>N10000  <span>/one-off</span></h4> 
+                               </Card.Header>
+                               <Card.Body>
+                                   <div className="card-div">
+                                     <h6 className="green">Direction Plan</h6>
+                                     <p>pilnar pivinar tempus o tempor impermjidiet mattis acumsan nulia vel</p>
+                                   </div>
+                                   <ul className="card-list">
+                                       <li><img src={mark_green} className="card-image" />Everything under <span className="blue">INSIGHT</span></li>
+                                       <li><img src={mark_green} className="card-image"/>40 minutes career-planning session with a clarity counsellor</li>
+                                   </ul>
+                                   <span className="card_btn btn-green card_btn--animated">Upgrade to Diamond</span>
+                               </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="centered_payment">
+                        <Col md={3}>
+                            <Card className="subcription-card">
+                                <Card.Header>
+                                    <h6 className="red">Gift a Subscription</h6> 
+                                </Card.Header>
+                                <Card.Body>
+                                   <p className="sub-p">Give a gift of success. Choose from among the three different plans</p>
+                                   <span className="card_btn btn-red card_btn--animated">Give a Clarity Subscription</span>    
+                                </Card.Body>
+                            </Card>
                         </Col>
                     </Row>
                 </Container>
