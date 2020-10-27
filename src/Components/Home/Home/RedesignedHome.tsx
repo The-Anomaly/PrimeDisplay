84d8135d379ec539b7332fca99f740cedaefd97b
The Anomaly <favouradekogbe@gmail.com>
2020-10-27 10:01:02 +0100

    Added landing page redesign components

--- src/Components/Home/Home/RedesignedHome.tsx
+++ src/Components/Home/Home/RedesignedHome.tsx
@@ -4,15 +4,6 @@ import Navbar from "../HomeComponents/newnavbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import RightTopImage from "../../../assets/2.png";
-import testimonial from "../../../assets/testimonial.png";
-import likegoogle from "../../../assets/1.png";
-import howitwk from "../../../assets/3.jpg";
-import Footer from "../HomeComponents/footer";
-import TakeAssessment from "../HomeComponents/TakeAssesment/takeAssessment";
-import stepgood from "../../../assets/stepgood.png";
-import stepone from "../../../assets/steptwo.png";
-import steptwo from "../../../assets/stepthree.png";
 import { Link } from "react-router-dom";
 import "../../../Fonts/Brfirma/BRFirma-Regular.ttf";
 import "../../../Fonts/Brfirma/BRFirma-Bold.ttf";
@@ -24,12 +15,20 @@ import insta from "../../../assets/Vectorinsta.png";
 import linkedin from "../../../assets/Vectorlinkedin.png";
 import twitter from "../../../assets/Vectortwitter.png";
 import rect from "../../../assets/Landingrectangle.png";
-import circle1 from "../../../assets/circle1.svg"
-import circle2 from "../../../assets/circle2.svg"
-import arrow1 from "../../../assets/arrow1.svg"
-import circle3 from "../../../assets/circle3.svg"
-import arrow2 from "../../../assets/arrow2.svg"
-import arrow3 from "../../../assets/arrow3.svg"
+import circle1 from "../../../assets/circle1.svg";
+import circle2 from "../../../assets/circle2.svg";
+import arrow1 from "../../../assets/arrow1.svg";
+import circle3 from "../../../assets/circle3.svg";
+import arrow2 from "../../../assets/arrow2.svg";
+import arrow3 from "../../../assets/arrow3.svg";
+import circle4 from "../../../assets/circle4.svg";
+import circle5 from "../../../assets/circle5.svg";
+import arrow4 from "../../../assets/arrow4.svg";
+import circle6 from "../../../assets/circle6.svg";
+import arrow5 from "../../../assets/arrow5.svg";
+import ClientReview from "../HomeComponents/HearFromClients";
+import Think from "../HomeComponents/ThinkThisIsYou";
+import Footer from "../HomeComponents/newfooter";
 
 const RedesignedHome: React.FC = () => {
   const [name, setName] = React.useState("");
@@ -66,7 +65,7 @@ const RedesignedHome: React.FC = () => {
             </span>
           </div>
           <div className="crazycircle1">
-            <img src={circle1} className="lcircle1"/>
+            <img src={circle1} className="lcircle1" />
             <img src={arrow1} className="larrow1" />
             <img src={circle2} className="lcircle2" />
           </div>
@@ -75,7 +74,88 @@ const RedesignedHome: React.FC = () => {
             <img src={circle3} className="lcircle3" />
             <img src={arrow3} className="larrow3" />
           </div>
+          <div className="crazycircle3">
+            <img src={circle4} className="lcircle4" />
+            <img src={circle5} className="lcircle5" />
+          </div>
+          <div className="crazycircle4">
+            <img src={arrow4} className="larrow4" />
+            <img src={circle6} className="lcircle6" />
+            <img src={arrow5} className="larrow5" />
+          </div>
         </Row>
+        <Row className="newthirdrow">
+          <div className="newthirdrowtext">
+            Clarity by Yudimy is a career mapping platform that helps you
+            discover and define a clear road map with practical and actionable
+            steps to get to your destination of meaningful and organized
+            work-life.
+          </div>
+          <div className="newthirdrowbtn">GET STARTED</div>
+        </Row>
+        <Row className="newfourthrow">
+          <div className="secondtitle fourthttl">
+            A navigator you can{" "}
+            <span className="secondtitle1">
+              trust <img src={rect} className="secondimg fourthimg" />
+            </span>
+          </div>
+          <div className="fourthline"> </div>
+          <div className="fourthtext">
+            <div>Are you struggling to figure out your next career move?</div>
+            <div>Clarity by Yudimy is for you.</div>
+          </div>
+          <div>
+            <div className="fourthsec">
+              <div className="fourthsubsec">
+                <div className="fourthsubhead">
+                  Career & Competence Evaluation Assessment
+                </div>
+                <div className="fourthsubtext">
+                  This one of a kind assessment tells if you are on the right
+                  career track or not, the competencies you need to develop, the
+                  best career-business expressions for your talent and the kind
+                  of roles you should work in.
+                </div>
+              </div>
+              <div className="fourthsubsec">
+                <div className="fourthsubhead">Talk to a Counselor</div>
+                <div className="fourthsubtext">
+                  Your career trajectory is a sum total of all the choices you
+                  made that got you here. However, an important step for making
+                  effective decisions going forward is is connecting the dots,
+                  speaking with a clarity counsellor helps you do just this with
+                  specific recommendations on how.{" "}
+                </div>
+              </div>
+            </div>
+            <div className="fourthsec">
+              <div className="fourthsubsec">
+                <div className="fourthsubhead">TO DO’s</div>
+                <div className="fourthsubtext">
+                  Knowing the next step to take is one, getting there is another
+                  thing. With Yudimy’s Clarity you can immediately convert the
+                  recommendations from your counselling session to actionable
+                  task with frequent reminders and acccountability to help you
+                  achieve this.
+                </div>
+              </div>
+              <div className="fourthsubsec">
+                <div className="fourthsubhead">Growth Challenge</div>
+                <div className="fourthsubtext">
+                  Beautiful things happen when preparation meets opportunity and
+                  our growth challenge feature keeps you accountable to growth,
+                  with the assistance of career-talent managers you have access
+                  to a career development road map that increase your appeal.
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </Row>
+        <ClientReview />
+        <Think />
+        <Footer />
       </Container>
     </div>
   );
---
+++ src/Components/Home/HomeComponents/ThinkThisIsYou.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Row } from "react-bootstrap";
+import "../Home/Home.css";
+
+
+const ThinkThisIsYou = () => {
+  return (
+    <>
+    <Row className="homethink">
+      <div className="homethinktxt">think this is you? lets help you fix that.<br />Take the free assesmenT</div>
+      <div className="homethinkbtn">Get Started</div>
+    </Row>
+    </>
+  );
+};
+
+export default ThinkThisIsYou;
