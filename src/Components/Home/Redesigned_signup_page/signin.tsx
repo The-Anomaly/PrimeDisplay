29574f9879ae2aa78c6d5102fbac851b17dafbc7
Hilary Israel <israel.hilary7@yahoo.com>
2020-11-14 14:48:52 +0100

    sign up hide password functionality added

--- src/Components/Home/Redesigned_signup_page/signin.tsx
+++ src/Components/Home/Redesigned_signup_page/signin.tsx
@@ -1,66 +1,174 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 import Navbar from "../HomeComponents/newnavbar";
 import "./signup.css";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Container, Row, Col, Form, Alert } from "react-bootstrap";
-import axios from 'axios';
-import { API } from '../../../config';
+import axios from "axios";
+import { API } from "../../../config";
+import eye from "../../../assets/eye.png";
+import eye12 from "../../../assets/eye-off.png";
 
-
-
-const Signin = ()=>{
-  const [ state, setState ] = useState({
-    email: '',
-    password: '',
-    errorMessage: '',
-    successMessage: '',
+const Signin = withRouter((props: any) => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    errorMessage: "",
+    successMessage: "",
     isLoading: false,
-    error: false
+    passwordIsOpen: false,
+    error: false,
   });
-  const {email, password,error, errorMessage, successMessage, isLoading} = state
+  const {
+    email,
+    password,
+    passwordIsOpen,
+    error,
+    errorMessage,
+    successMessage,
+    isLoading,
+  } = state;
   const sendFormData = () => {
     setState({
-    ...state,
-    isLoading: true
+      ...state,
+      isLoading: true,
     });
-    const data ={
-    email,
-    password
+    const data = {
+      email,
+      password,
     };
-   axios.post(`${API}/accounts/login`, data )
-   .then((response) => {
-     if (response.status === 200){
-         
-      setState ({
-       ...state,
-       successMessage: response.data[0].message,
-       isLoading: false 
-     });
+    axios
+      .post(`${API}/accounts/login`, data)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          localStorage.setItem(
+            "userToken",
+            JSON.stringify(response?.data?.token)
+          );
+          getUserInfo(response.data.token);
+          getCurrentAssessmentPosition(response.data.token);
+        }
+        setState({
+          ...state,
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error) {
+          return setState({
+            ...state,
+            errorMessage: error?.response?.data[0].message,
+            isLoading: false,
+            error: true,
+          });
+        }
+      });
+  };
+  const onChangeHandler = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+      errorMessage: "",
+    });
+  };
+  const hidePassword = () => {
+    setState({
+      ...state,
+      passwordIsOpen: state.passwordIsOpen ? false : true,
+    });
+  };
+  const getCurrentAssessmentPosition = (token: string): void => {
+    axios
+      .get(`${API}/progress`, { headers: { Authorization: `Token ${token}` } })
+      .then((response) => {
+        if (
+          (response.status === 200 &&
+            response.data[0].next === "phase_four_nature") ||
+          response.data[0].next === "phase_four_health" ||
+          response.data[0].next === "phase_four_building" ||
+          response.data[0].next === "phase_four_creative"
+        ) {
+          return props.history.push(`/assessmentphasefour`);
+        }
+        if (
+          (response.status === 200 &&
+            response.data[0].next === "phase_four_sports") ||
+          response.data[0].next === "phase_four_business" ||
+          response.data[0].next === "phase_four_stem" ||
+          response.data[0].next === "phase_four_humanitarian"
+        ) {
+          return props.history.push(`/assessmentphasefour1`);
+        }
+        if (response.status === 200 && response.data[0].next === "phase_one") {
+          return props.history.push(`/assessmentphaseone`);
+        }
+        if (
+          response.status === 200 &&
+          response.data[0].next === "onboarding_chat"
+        ) {
+          return props.history.push(`/clientchat`);
+        }
+        if (response.status === 200 && response.data[0].next === "phase_two") {
+          return props.history.push(`/assessmentphasetwo`);
+        }
+        if (
+          response.status === 200 &&
+          response.data[0].next === "phase_three"
+        ) {
+          return props.history.push(`/assessmentphasethree`);
+        }
+        if (response.status === 200 && response.data[0].next === "phase_five") {
+          return props.history.push(`/assessmentphasefive`);
+        }
+        if (response.status === 200 && response.data[0].next === "phase_six") {
+          return props.history.push(`/assessmentphasesix`);
+        }
+        if (
+          response.status === 200 &&
+          response.data[0].next === "phase_seven"
+        ) {
+          return props.history.push(`/assessmentphaseseven`);
+        }
+        if (response.status === 200 && response.data[0].next === "home") {
+          return props.history.push(`/free/dashboard`);
+        }
+      })
+      .catch((error) => {});
+  };
+  const getUserInfo = (token: string): any => {
+    axios
+      .get(`${API}/currentuser`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          localStorage.setItem("user", JSON.stringify(response?.data));
+        }
+      })
+      .catch((error) => {});
+  };
+  const validateForm = (e) => {
+    e.preventDefault();
+    if (email === "") {
+      return setState({
+        ...state,
+        errorMessage: "Please enter your email",
+      });
     }
-   })
-   .catch((error)=>{
-     console.log(error.response)
-     if (error){
-       return setState({
-         ...state,
-         errorMessage:error?.response?.data[0].message,
-         isLoading: false,
-         error: true
-       });
-     };
-   });
-}
- const onChangeHandler= (e)=>{
-   setState({
-     ...state,
-     [e.target.name]: e.target.value
-   });
- };
-
-    return(
-        <div>
-             <Navbar />
-             <div className="rdsignup-section">
+    if (password === "") {
+      return setState({
+        ...state,
+        errorMessage: "Please enter your password",
+      });
+    } else {
+      sendFormData();
+    }
+  };
+  return (
+    <div>
+      <Navbar />
+      <div className="rdsignup-section">
         <Container>
           <Row className="rsignuprow">
             <Col md={12} className="rsignupdiv">
@@ -77,7 +185,7 @@ const Signin = ()=>{
               </p>
             </Col>
             <Col md={7}>
-              <Form className="rdsignupform">
+              <Form className="rdsignupform" onSubmit={validateForm}>
                 <div className="rdsignupfrmdv">
                   <h4 className="sgnfrmhder">Log In</h4>
                   <div>
@@ -86,14 +194,22 @@ const Signin = ()=>{
                   </div>
                 </div>
                 {successMessage && (
-                <Alert key={2} variant="success" className=" alertzuccess" >
-                   {successMessage}
-                </Alert>
+                  <Alert
+                    key={2}
+                    variant="success"
+                    className=" alertzuccess text-center"
+                  >
+                    {successMessage}
+                  </Alert>
                 )}
                 {errorMessage && (
-                <Alert key={2} variant="danger" className="alertzuccess" >
-                   {errorMessage}
-                </Alert>
+                  <Alert
+                    key={2}
+                    variant="danger"
+                    className="alertzuccess text-center"
+                  >
+                    {errorMessage}
+                  </Alert>
                 )}
                 <label>
                   <span className="rdfrmlbl"> Email Address </span>
@@ -110,20 +226,44 @@ const Signin = ()=>{
                 <label>
                   <span className="rdfrmlbl">Password</span>
                   <input
-                    type="password"
+                    type={passwordIsOpen ? "password" : "text"}
                     name="password"
                     value={password}
                     onChange={onChangeHandler}
                     placeholder="Enter your Password"
                     size={75}
-                    className="form-control rdsignupinput passwd-image"
+                    className="form-control rdsignupinput"
                   />
                 </label>
+                <div className="text-right">
+                  {passwordIsOpen ? (
+                    <img
+                      src={eye}
+                      className="hideeye"
+                      onClick={hidePassword}
+                      alt="hideeye"
+                    />
+                  ) : (
+                    <img
+                      src={eye}
+                      className="hideeye"
+                      onClick={hidePassword}
+                      alt="hideeye"
+                    />
+                  )}
+                </div>
                 <div className="rdsigninp">
-                    <p><Link to="/signin">Forgot your password?</Link></p>
+                  <p>
+                    <Link to="/signin">Forgot your password?</Link>
+                  </p>
                 </div>
                 <div className="rdsgnupfrmbtndv">
-                  <span className="rdsgnfrmbtn rdsgnup-animated" onClick={sendFormData}>Log In</span>
+                  <span
+                    onSubmit={validateForm}
+                    className="rdsgnfrmbtn rdsgnup-animated"
+                  >
+                    {isLoading ? "Processing" : "Log In"}
+                  </span>
                 </div>
                 <p className="rdsgnalready">
                   Don't have an account? <Link to="/signup">Sign Up</Link>
@@ -133,8 +273,8 @@ const Signin = ()=>{
           </Row>
         </Container>
       </div>
-   </div>
-    )
-}
+    </div>
+  );
+});
 
 export default Signin;
