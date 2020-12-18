6674bb1d3a6e6d546379a95beb67c918fe4d82df
The Anomaly <favouradekogbe@gmail.com>
2020-12-18 10:14:16 +0100

    switched to production server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-// export const API = "https://clarityappapi.herokuapp.com" 
+export const API = "https://clarityappapi.herokuapp.com" 
 
-// export const webSocketAPI = "clarityappapi.herokuapp.com";
-export const webSocketAPI = "https://claritystaging.herokuapp.com";
-export const API = "https://claritystaging.herokuapp.com"
+export const webSocketAPI = "clarityappapi.herokuapp.com";
+// export const webSocketAPI = "https://claritystaging.herokuapp.com";
+// export const API = "https://claritystaging.herokuapp.com"
 //"https://claritystaging.herokuapp.com" 
