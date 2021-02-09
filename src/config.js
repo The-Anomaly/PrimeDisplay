4818de2d7432c900d094bde96b5c5c33b8ecf395
Hilary <israel.hilary7@yahoo.com>
2021-02-09 10:46:54 +0100

    added counsellor assessment

--- src/config.js
+++ src/config.js
@@ -6,3 +6,4 @@ export const webSocketAPI = "clarityappapi.herokuapp.com";
 // export const webSocketAPI = "https://clarityappapi.herokuapp.com";
 // export const API = "https://clarityappapi.herokuapp.com"
 //"https://claritystaging.herokuapp.com"
+ 
