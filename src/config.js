6d91101b1fb5a78f5e38162fdb3e83e0adfc1f30
Hilary <israel.hilary7@yahoo.com>
2021-04-15 13:18:12 +0100

    added updates

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@
 
 // Clarity Staging API
 export const API = "https://claritydev.herokuapp.com";
-export const webSocketAPI = "https://claritydev.herokuapp.com";
+export const webSocketAPI = "claritydev.herokuapp.com";
