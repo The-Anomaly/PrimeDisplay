16d976241c5262c66058379447a470efaa90f519
The Anomaly <favouradekogbe@gmail.com>
2021-05-21 23:18:20 +0100

    changes

--- src/Components/Dashboard/Notification.tsx
+++ src/Components/Dashboard/Notification.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
 import "./kegennidashboard.css";
 import close from "../../assets/close.svg";
-import { useHistory }  from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Notification = (props: any) => {
-    const [state, setState] = React.useState(false)
-    const closeBanner = () => {
-        setState(true)
-    }
-    let history = useHistory();
-    const viewPlans = () => {
-        return history.push("/paymentsummary");
-    }
-    return (
-        <>
-        {!state && (<div className="notification-banner">
-            <span>New pricing plans available till the 31st of May. Don't miss out!</span> <button onClick={viewPlans}>View plans</button> <img onClick={closeBanner} className="close-notif" src={close} alt="" />
-        </div>)}
-        </>
-    )
-}
+  const [state, setState] = React.useState(false);
+  const closeBanner = () => {
+    setState(true);
+  };
+  let history = useHistory();
+  const viewPlans = () => {
+    return history.push("/paymentsummary");
+  };
+  return (
+    <>
+      {!state && (
+        <div className="notification-banner">
+          <span>
+            New pricing plans available for a limited time. Don't miss out!
+          </span>{" "}
+          <button onClick={viewPlans}>View plans</button>{" "}
+          <img
+            onClick={closeBanner}
+            className="close-notif"
+            src={close}
+            alt=""
+          />
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Notification;
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 // Clarity Production API
-// export const API = "https://clarityappapi.herokuapp.com";
-// export const webSocketAPI = "clarityappapi.herokuapp.com";
-
+export const API = "https://clarityappapi.herokuapp.com";
+export const webSocketAPI = "clarityappapi.herokuapp.com";
 
 // Clarity Staging API
-export const API = "https://claritydev.herokuapp.com";
-export const webSocketAPI = "claritydev.herokuapp.com";
+// export const API = "https://claritydev.herokuapp.com";
+// export const webSocketAPI = "claritydev.herokuapp.com";
